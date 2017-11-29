ac71964ba04312178da6523befe4c41b627310a9
Kris <kristopher_anderson@icloud.com>
2017-11-29 11:42:47 +0000

    Test pub has name.

--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -18,9 +18,9 @@ end
 
 
 
+# # A Drink should have a name, and a price
+# # A Customer should have a name, and a wallet
 # A Pub should have a name, a till, and a collection of drinks
-# A Drink should have a name, and a price
-# A Customer should have a name, and a wallet
 # A Customer should be able to buy a Drink from the Pub,
 # reducing the money in its wallet and
 # increasing the money in the Pub's till
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -4,4 +4,12 @@ require_relative("../pub")
 
 class TestPub < MiniTest::Test
 
+  # A Pub should have a name, a till, and a collection of drinks
+  def test_pub_has_name
+    cloisters = Pub.new("Cloisters", 250, [])
+    expected = "Cloisters"
+    result = cloisters.name
+    assert_equal(expected, result)
+  end
+
 end
