29ac805b0a328b6588deff733a3d41ff599a16ff
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-11-29 19:13:45 +0000

    test_drink_has_alcohol_level

--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -4,24 +4,30 @@ require_relative("../drink")
 
 class TestDrink < MiniTest::Test
 
+# A Drink should have a name,
+# a price and alcohol level
+  def setup
+    @drink = Drink.new("Tennants", 5, 50)
+  end
+
   def test_drink_has_name
-    drink = Drink.new("Tennants", 5)
-    assert_equal("Tennants", drink.name)
+    assert_equal("Tennants", @drink.name)
   end
 
   def test_drink_has_price
-    drink = Drink.new("Tennants", 5)
-    assert_equal(5, drink.price)
+    assert_equal(5, @drink.price)
   end
 
-
+  def test_drink_has_alcohol_level
+    assert_equal(50, @drink.alcohol_level)
+  end
 end
 
 
 
-# # A Drink should have a name, and a price
+
 # # A Customer should have a name, and a wallet
 # #A Pub should have a name, a till, and a collection of drinks
-C# A Customer should be able to buy a Drink from the Pub,
+# A Customer should be able to buy a Drink from the Pub,
 # reducing the money in its wallet and
 # increasing the money in the Pub's till
