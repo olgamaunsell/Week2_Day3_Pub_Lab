207e29123ae092d660029444c7baf709ae624c5a
Kris <kristopher_anderson@icloud.com>
2017-11-29 11:21:34 +0000

    test_customer_has_name has passed

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -3,5 +3,15 @@ require("minitest/rg")
 require_relative("../customer")
 
 class TestCustomer < MiniTest::Test
+
+  def test_customer_has_name
+    customer = Customer.new("Olga", 30)
+    assert_equal("Olga", customer.name)
+  end
+
+  # def test_drink_has_price
+  #   drink = Drink.new("Tennants", 5)
+  #   assert_equal(5, drink.price)
+  # end
   
 end
