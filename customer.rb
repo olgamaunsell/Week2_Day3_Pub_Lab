daf1b263c37d111b450d26c914774e502498843c
Kris <kristopher_anderson@icloud.com>
2017-11-29 15:04:28 +0000

    missing files

---
+++ customer.rb
@@ -0,0 +1,33 @@
+class Customer
+
+  attr_reader :name, :wallet, :age
+
+  def initialize(name, wallet, age)
+    @name = name
+    @wallet = wallet
+    @age = age
+  end
+
+  def can_afford?(drink)
+    # Check if customer's wallet is greater than or equal
+    # to the drink price
+    # @wallet >= drink.price
+    return true if @wallet >= drink.price
+    return false if @wallet < drink.price
+  end
+
+  def buy_drink(pub, drink)
+    # check if pub will serve customer
+    # check if customer can afford drink
+    if can_afford?(drink) && pub.will_serve?(@age)
+      # step 1. customer wallet reduces by price of drink.
+      @wallet -= drink.price
+      #step 2. pub till increases by drink sold value.
+      pub.add_money(drink.price)
+    else
+      return "Transaction declined"
+    end
+  end
+
+
+end
---
+++ pub.rb
@@ -0,0 +1,36 @@
+class Pub
+
+  attr_reader :name, :till, :drinks
+
+  def initialize(name, till, drinks)
+    @name = name
+    @till = till
+    @drinks = drinks
+  end
+
+  def add_money(amount)
+    @till += amount
+  end
+
+  def will_serve?(age)
+    # Pub checks customer age before serving
+    return true if age >= 18
+    return false if age < 18
+  end
+end
+
+# A Pub should have a name, a till, and a collection of drinks
+
+
+# #The method add_money acts on the pub to add
+# #the value of the drink to the till
+# till_expected = 1006
+# actual_till = pub.add_money(drink.price)
+# assert_equal(till_expected, actual_till)
+
+
+# #The method add_money acts on the pub to add
+# #the value of the drink to the till
+# till_expected = 1006
+# actual_till = pub.add_money(drink.price)
+# assert_equal(till_expected, actual_till)
